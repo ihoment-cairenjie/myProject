069fb033e83b1e6febd5f13a83afdc35a38a198c
ihoment-cairenjie <renjie.cai@ihoment.com>
2021-03-03 12:08:26 +0800

    添加依赖

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="nav">
+    <Test></Test>
+  </div>
+</template>
+
+<script>
+import {
+  onMounted,
+  onBeforeMount
+
+} from 'vue'
+import Test from '@/components/Test'
+export default {
+  name: 'App',
+  components: {
+    Test
+  },
+  setup (props, ctx){
+
+    onMounted(()=>{
+      console.log('onMounted')
+    })
+
+    onBeforeMount(()=>{
+      console.log('onBeforeMount')
+    })
+
+    console.log('setups')
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+  testContent
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
